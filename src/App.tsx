1eab9bab83ae021080e77bcf565300b03a2cb9ca
raihan2bd <raihan2bd.official@gmail.com>
2023-08-26 10:42:45 +0600

    Initialize the pages into the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import {Routes, Route} from 'react-router-dom';
+
 import Layout from "./components/Layout/Layout"
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import MoviesPage from './pages/MoviesPage';
+import SingleMoviePage from './pages/SingleMoviePage';
+import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:id" element={<SingleMoviePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </Layout>
   )
 }
