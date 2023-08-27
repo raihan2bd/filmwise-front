4294aa295c9382456323fb8d5bb8b275450f9ae1
raihan2bd <raihan2bd.official@gmail.com>
2023-08-27 23:22:28 +0600

    Fix redux store import issue from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import { store } from "./redux/store.tsx";
+import {store} from './redux/store'
 
 import App from "./App.tsx";
 import "./index.css";
