41aa97faee090886bf500441041b4296f5ecdc47
raihan2bd <raihan2bd.official@gmail.com>
2023-08-28 22:01:28 +0600

    Add feature movies component and render it on Home page

---
+++ src/components/FeatureMovies/ActionMovies.tsx
@@ -0,0 +1,33 @@
+import {useGetActionMoviesQuery} from '../../redux/services/featureMoviesApi'
+import Spinner from '../UI/Spinner';
+
+import MoviesCarousel from './MoviesCarousel';
+
+const ActionMovies = () => {
+
+  const {isLoading, isSuccess, isError, data: featureMovies, error} = useGetActionMoviesQuery()
+
+  let content;
+  if(isLoading) {
+    content = <Spinner />
+  } else if(isError) {
+    let errorMessage;
+
+    if ('data' in error && error.data) {
+      errorMessage = error.data.error.message;
+    } else {
+      errorMessage = 'An unknown error occurred. Please try again.';
+    }
+
+    content = <p className="text-red-500 text-lg shadow">{errorMessage}</p>
+  } else if(isSuccess && featureMovies.movies) {
+    console.log(featureMovies)
+    content = <MoviesCarousel movies={featureMovies.movies} />
+  }
+
+  return (
+    <div className="min-h-[100px] flex flex-col justify-center items-center">{content}</div>
+  )
+}
+
+export default ActionMovies;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,33 @@
 import ImageSlider from "../components/ImageSlider/ImageSlider";
+import PopularMovies from "../components/FeatureMovies/PopularMovies";
+import ActionMovies from "../components/FeatureMovies/ActionMovies";
+import DramaMovies from "../components/FeatureMovies/DramaMovies";
+import MysteryMovies from "../components/FeatureMovies/MysteryMovies";
 
 const HomePage = () => {
+  const sectionClasses = 'bg-white/5 mt-5 p-4';
+  const titleClasses = 'text-xl font-bold text-yellow-500 border-s-4 ps-4 my-4'
   return (
     <>
       <ImageSlider />
-      <section className="">
-        <h2>Popular Movies</h2>
-        <div>content</div>
+      <section className={sectionClasses}>
+        <h2 className={titleClasses}>Popular Movies</h2>
+        <PopularMovies />
       </section>
-      <section className="">
-      <h2>Popular Movies</h2>
-        <div>content</div>
+      
+      <section className={sectionClasses}>
+        <h2 className={titleClasses}>Action Movies</h2>
+        <ActionMovies />
       </section>
-      <section className="">
-      <h2>Popular Movies</h2>
-        <div>content</div>
+
+      <section className={sectionClasses}>
+        <h2 className={titleClasses}>Drama Movies</h2>
+        <DramaMovies />
+      </section>
+
+      <section className={sectionClasses}>
+        <h2 className={titleClasses}>Drama Movies</h2>
+        <MysteryMovies />
       </section>
     </>
   );
