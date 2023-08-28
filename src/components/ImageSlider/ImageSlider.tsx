d4babd15db935ae630144feba22cfa90596cb456
raihan2bd <raihan2bd.official@gmail.com>
2023-08-28 21:59:22 +0600

    Fix image slider

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,7 +1,7 @@
 import AliceCarousel from 'react-alice-carousel';
 import { useGetFeatureMoviesQuery } from '../../redux/services/featureMoviesApi';
 
-import Forrest_gump_img from '../../assets/images/forrest_gump.jpg';
+import noThumbnail from '../../assets/images/no_thumbnail.jpg';
 
 import classes from './ImageSlider.module.css';
 import 'react-alice-carousel/lib/alice-carousel.css';
@@ -17,7 +17,7 @@ const ImageSlider = () => {
     const items = featureMovies.movies.map((movie) => (
       <div key={movie.id} className={classes.slider_item} data-value={movie.id}>
         <div className={classes.slider_image}>
-          <img src={movie.image !== "" || movie.image ? movie.image : Forrest_gump_img} alt={movie.title} />
+          <img src={movie.image !== "" || movie.image ? movie.image : noThumbnail} alt={movie.title} />
         </div>
         <div className={classes.slider_title}>
           <a className={classes.slider_title_link} href={`/movies/${movie.id}`}>
