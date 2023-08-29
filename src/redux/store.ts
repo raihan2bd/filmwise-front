0d84d66d20175321b0b779dc2a4ffd07f2281c34
raihan2bd <raihan2bd.official@gmail.com>
2023-08-29 15:10:53 +0600

    Update the redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./features/authSlice";
 import {featureMoviesApi} from './services/featureMoviesApi';
+import { moviesApi } from "./services/movieApi";
 
 export const store = configureStore({
-  reducer: {auth: authSlice, [featureMoviesApi.reducerPath]: featureMoviesApi.reducer},
+  reducer: {auth: authSlice, [featureMoviesApi.reducerPath]: featureMoviesApi.reducer, [moviesApi.reducerPath]: moviesApi.reducer},
   devTools: import.meta.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([
     featureMoviesApi.middleware,
+    moviesApi.middleware,
   ]),
 });
