016ef0fe68d9658d52f89791765f32caedb82935
raihan2bd <raihan2bd.official@gmail.com>
2023-09-01 13:19:13 +0600

    Modify the custom button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -7,8 +7,10 @@ type PropsType = {
   children: ReactNode,
 };
 
+;
+
 const Button = ({ btnClass, type = "button", onClickHandler, children }: PropsType) => {
-  const btnClasses = btnClass ? `btn-default ${btnClass}` : `btn-default`;
+  const btnClasses = btnClass ? `text-sm text-white bg-lime-500 hover:bg-lime-700 active:bg-lime-500 px-4 py-2 rounded-sm disabled:opacity-25 disabled:cursor-not-allowed ${btnClass}` : `text-sm text-white bg-lime-500 hover:bg-lime-700 active:bg-lime-500 px-4 py-2 rounded-sm disabled:opacity-25 disabled:cursor-not-allowed`;
 
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
     if (onClickHandler) {
@@ -17,7 +19,7 @@ const Button = ({ btnClass, type = "button", onClickHandler, children }: PropsTy
   };
 
   return (
-    <button className={btnClasses} type={type} onClick={onClickHandler ? handleClick : undefined}>
+    <button className={btnClasses} type={type? type : 'button'} onClick={onClickHandler ? handleClick : undefined}>
       {children}
     </button>
   );
