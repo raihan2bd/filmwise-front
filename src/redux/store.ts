273bdf35ff912d05ffd07b12bfc21e9b13dd2540
raihan2bd <raihan2bd.official@gmail.com>
2023-09-01 21:47:08 +0600

    Add root store type to the project

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,16 +4,23 @@ import { featureMoviesApi } from './services/featureMoviesApi';
 import { moviesApi } from "./services/movieApi";
 import { genresApi } from './services/genresApi';
 
+export interface RootStoreType {
+  auth: ReturnType<typeof authSlice>;
+  featureMovies: ReturnType<typeof featureMoviesApi.reducer>;
+  movies: ReturnType<typeof moviesApi.reducer>;
+  genres: ReturnType<typeof genresApi.reducer>;
+}
+
 export const store = configureStore({
   reducer: {
     auth: authSlice,
-    [featureMoviesApi.reducerPath]: featureMoviesApi.reducer, // Check if these paths align with your slice configurations
+    [featureMoviesApi.reducerPath]: featureMoviesApi.reducer,
     [moviesApi.reducerPath]: moviesApi.reducer,
     [genresApi.reducerPath]: genresApi.reducer,
   },
   devTools: import.meta.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([ // Consider using the default middleware here
+    getDefaultMiddleware().concat([
       featureMoviesApi.middleware,
       moviesApi.middleware,
       genresApi.middleware,
