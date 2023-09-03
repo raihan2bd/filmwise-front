650636b964c8bd6fda4134afd045a557f60b2620
raihan2bd <raihan2bd.official@gmail.com>
2023-09-03 11:04:19 +0600

    Fix bugs from useInput hooks

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,25 +1,23 @@
-import { useReducer, ChangeEvent, FocusEvent } from 'react';
+import { useReducer, ChangeEvent } from 'react';
 
 interface InputState {
   value: string;
   isTouched: boolean;
-  errorMsg: string | null;
 }
 
 type InputAction =
-  | { type: 'INPUT'; value: string; errorMsg: string | null }
+  | { type: 'INPUT'; value: string; }
   | { type: 'BLUR' }
   | { type: 'RESET' };
 
 const initialInputState: InputState = {
   value: '',
   isTouched: false,
-  errorMsg: null,
 };
 
 const inputStateReducer = (state: InputState, action: InputAction): InputState => {
   if (action.type === 'INPUT') {
-    return { ...state, value: action.value, errorMsg: action.errorMsg };
+    return { ...state, value: action.value };
   }
   if (action.type === 'BLUR') {
     return { ...state, isTouched: true };
@@ -36,12 +34,11 @@ const useInput = (validateValue: (value: string) => string | null) => {
     initialInputState
   );
 
-  const valueIsValid = inputState.errorMsg === null;
+  const errorMsg = validateValue(inputState.value);
 
   const valueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    const errorMsg = validateValue(value);
-    dispatch({ type: 'INPUT', value, errorMsg });
+    dispatch({ type: 'INPUT', value });
   };
 
   const inputBlurHandler = () => {
@@ -54,8 +51,7 @@ const useInput = (validateValue: (value: string) => string | null) => {
 
   return {
     value: inputState.value,
-    isValid: valueIsValid,
-    errorMsg: inputState.errorMsg,
+    errorMsg,
     isTouched: inputState.isTouched,
     valueChangeHandler,
     inputBlurHandler,
