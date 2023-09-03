d07d2fbdf523b6f18ff0d4b57fd490234acbcd08
raihan2bd <raihan2bd.official@gmail.com>
2023-09-03 11:03:41 +0600

    Inhance custom button component

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,25 +1,18 @@
-import { ReactNode, MouseEvent } from 'react';
+import { ReactNode, ButtonHTMLAttributes } from 'react';
 
 type PropsType = {
   btnClass?: string,
   type?: "button" | "submit" | "reset",
-  onClickHandler?: (event: MouseEvent<HTMLButtonElement>) => void,
   children: ReactNode,
-};
+}& ButtonHTMLAttributes<HTMLButtonElement>;
 
 ;
 
-const Button = ({ btnClass, type = "button", onClickHandler, children }: PropsType) => {
+const Button = ({ btnClass, type = "button", children, ...restProps }: PropsType) => {
   const btnClasses = btnClass ? `text-sm text-white bg-lime-500 hover:bg-lime-700 active:bg-lime-500 px-4 py-2 rounded-sm disabled:opacity-25 disabled:cursor-not-allowed ${btnClass}` : `text-sm text-white bg-lime-500 hover:bg-lime-700 active:bg-lime-500 px-4 py-2 rounded-sm disabled:opacity-25 disabled:cursor-not-allowed`;
 
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    if (onClickHandler) {
-      onClickHandler(event);
-    }
-  };
-
   return (
-    <button className={btnClasses} type={type? type : 'button'} onClick={onClickHandler ? handleClick : undefined}>
+    <button className={btnClasses} type={type? type : 'button'}{...restProps}>
       {children}
     </button>
   );
