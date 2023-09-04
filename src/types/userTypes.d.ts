f11cd41ab10a7adfda78b658b329abfda2e6b596
raihan2bd <raihan2bd.official@gmail.com>
2023-09-04 19:12:36 +0600

    Update userTypes

--- src/types/userTypes.d.ts
+++ src/types/userTypes.d.ts
@@ -3,8 +3,16 @@ interface UserType {
   name: string,
   role: string,
   token: string,
+  expirationTime: number,
 }
 
 interface AuthStateType {
   user: UserType | null
+  userId: number | string | null
+}
+
+interface UserSignupDataType {
+  full_name: string, 
+  email: string, 
+  password: string
 }
