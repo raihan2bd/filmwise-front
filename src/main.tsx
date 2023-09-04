481e9c744a7dfd8c977e60631400cafd3d4d0c8c
raihan2bd <raihan2bd.official@gmail.com>
2023-09-04 22:51:35 +0600

    Retrive token from localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import {store} from './redux/store'
+import { retriveToken } from "./redux/features/authSlice.ts";
 
 import App from "./App.tsx";
 import "./index.css";
+
+store.dispatch(retriveToken())
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
