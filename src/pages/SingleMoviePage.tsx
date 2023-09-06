890d1ccb0af5b295aca4ca0b4d979ebb97e1cb84
raihan2bd <raihan2bd.official@gmail.com>
2023-09-06 20:24:50 +0600

    Add rating modal inside the single movie page

--- src/pages/SingleMoviePage.tsx
+++ src/pages/SingleMoviePage.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useCallback } from "react";
+import { useMemo, useCallback, useState } from "react";
 import { useParams, Link, useLocation, useNavigate } from "react-router-dom";
 
 import { useGetSingleMovieQuery } from "../redux/services/movieApi";
@@ -7,10 +7,14 @@ import Button from "../components/UI/Button";
 import AddCommentForm from "../components/Movies/AddCommentForm";
 import { formatDateDefault } from "../utils/utils";
 import { useAppSelector } from "../hooks/typeHooks";
+import Modal from "../components/UI/Modal";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const SingleMoviePage = () => {
+  const [showRatingModal, setShowRatingModal] = useState(false)
+
+
   const { id } = useParams();
   const user = useAppSelector((state) => state.auth.user);
   const { pathname, search } = useLocation();
@@ -23,6 +27,19 @@ const SingleMoviePage = () => {
     data: movieResponse,
     error,
   } = useGetSingleMovieQuery(Number(id));
+
+
+  const hideRatingModalHandler = () => {
+    setShowRatingModal(false)
+  }
+
+  const showRatingModalHandler = () => {
+    if (!user?.id) {
+      navigate(`/auth?callback=${pathname}${search}`);
+      return;
+    }
+    setShowRatingModal(true)
+  }
 
   const deleteMovieHandler = (id: number) => {
     console.log(id);
@@ -110,7 +127,7 @@ const SingleMoviePage = () => {
                 <Button onClick={() => handleAddFavorite(movie.id)}>
                   Add Favorite
                 </Button>
-                <Button onClick={() => handleAddRating(movie.id)}>
+                <Button onClick={showRatingModalHandler}>
                   Add Rating
                 </Button>
               </p>
@@ -194,7 +211,10 @@ const SingleMoviePage = () => {
     );
   }, [isLoading, isError, error, isSuccess, movieResponse]);
 
-  return <article className="p-4 bg-white/10">{content}</article>;
+  return <article className="p-4 bg-white/10">
+    {content}
+    {showRatingModal && <Modal onHandleClick={hideRatingModalHandler}><h4>Add Rating</h4></Modal>}
+    </article>;
 };
 
 export default SingleMoviePage;
