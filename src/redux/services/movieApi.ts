ca27e8a00e1c5b61aa3acf808bdafdb65ea1dcf4
raihan2bd <raihan2bd.official@gmail.com>
2023-09-09 23:34:09 +0600

    Update movie api

--- src/redux/services/movieApi.ts
+++ src/redux/services/movieApi.ts
@@ -1,10 +1,22 @@
 import { createApi, fetchBaseQuery, BaseQueryFn, FetchArgs} from '@reduxjs/toolkit/query/react'
+import { getTokenFromLocalStorage } from '../../utils/getToken';
 
 const baseApiUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const moviesApi = createApi({
   reducerPath: 'movies-api',
-  baseQuery: fetchBaseQuery({baseUrl: baseApiUrl}) as BaseQueryFn<string | FetchArgs, unknown, CustomErrorType, {}>,
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseApiUrl,
+    prepareHeaders: (headers) => {
+      const authToken = getTokenFromLocalStorage();
+
+      if (authToken) {
+        headers.set('Authorization', `Bearer ${authToken}`);
+      }
+
+      return headers;
+    },
+  }) as BaseQueryFn<string | FetchArgs, unknown, CustomErrorType, {}>,
   tagTypes: ['Movies-API'],
   endpoints: (builder) => ({
      
@@ -14,11 +26,23 @@ export const moviesApi = createApi({
 
     getSingleMovie: builder.query<MovieResponse, number>({
       query: (id) => `${baseApiUrl}/movie/get_one/${id}`
-    })
+    }),
+
+    createRating: builder.mutation<UpdateRatingResponse, RatingInputType>({
+      query: (ratingInput) => ({
+        url: `${baseApiUrl}/rating/add`,
+        method: 'POST',
+        body: ratingInput,
+      }),
+      invalidatesTags: [{ type: 'Movies-API'}],
+    }),
+
+
   })
 })
 
 export const {
  useGetFilteredMoviesQuery,
  useGetSingleMovieQuery,
+ useCreateRatingMutation,
 } = moviesApi;
