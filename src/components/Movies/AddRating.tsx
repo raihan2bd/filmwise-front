88ee047c75e25a1aff410108149465a21c384741
raihan2bd <raihan2bd.official@gmail.com>
2023-09-11 22:28:46 +0600

    Fix bugs from the add rating component

--- src/components/Movies/AddRating.tsx
+++ src/components/Movies/AddRating.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useCreateRatingMutation } from "../../redux/services/movieApi";
-import { useAppDispatch } from "../../hooks/typeHooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/typeHooks";
 import { logoutAction } from "../../redux/features/authSlice";
-
 
 interface RatingProps {
   maxStars: number;
@@ -11,76 +11,83 @@ interface RatingProps {
 }
 
 const AddRating: React.FC<RatingProps> = ({
-  maxStars=10,
+  maxStars = 10,
   setShowRatingModal,
   movieId,
 }) => {
-  const [createRating, { isLoading }] = useCreateRatingMutation()
-  const [hasError, setHasError] = useState<string | null>(null)
+  const [createRating, { isLoading }] = useCreateRatingMutation();
+  const [hasError, setHasError] = useState<string | null>(null);
   const [rating, setRating] = useState<number | null>(1);
-  const dispatch = useAppDispatch()
+
+  const user = useAppSelector((state) => state.auth.user)
+  const dispatch = useAppDispatch();
+  const { pathname, search } = useLocation();
+  const navigate = useNavigate();
 
   const handleStarClick = (starIndex: number) => {
     setRating(starIndex + 1);
   };
 
-  const handleAddRating = async() => {
+  const handleAddRating = async () => {
     if (rating === null) {
-      return
+      return;
     }
-
-    let response: any;
 
     try {
-        const ratingInput: RatingInputType = {
-          movie_id: movieId,
-          rating: rating,
-        };
-        response = await createRating(ratingInput)
-      if (response.data.id) {
-        setShowRatingModal(false)
-        return
+      const ratingInput: RatingInputType = {
+        movie_id: movieId,
+        rating: rating,
+      };
+      
+      await createRating(ratingInput).unwrap();
+
+      setShowRatingModal(false)
+    } catch (err) {
+      const error = err as CustomErrorType
+      if (error.status === 401) {
+        dispatch(logoutAction());
       } else {
-        throw new Error('Something went wrong. Please Try again!')
+        const errMsg =
+          error.data?.error?.message ||
+          "Something went wrong. Please try again.";
+        setHasError(errMsg);
       }
-
-    } catch(err) {
-      let errMsg = err instanceof Error ? err.message : 'Something went wrong. Please try again.'
-      const myErr: any = response
-
-      if(myErr.status === 401) {
-        dispatch(logoutAction())
-      }
-      if(myErr.error.data.error) {
-        errMsg = myErr.error.data.error.message
-      }
-
-      setHasError(errMsg)
     }
+    
   };
 
   const handleCancel = () => {
-    setShowRatingModal(false)
+    setShowRatingModal(false);
   };
+
+  useEffect(() => {
+    if(!user) {
+      navigate(`/auth?callback=${pathname}${search}`);
+    }
+  }, [user])
 
   return (
     <>
-    <div className="rating flex-col items-center justify-center w-fit mx-auto text-center">
-      {hasError && <p className="pb-2 text-red-500 p-2 text-center text-sm" >{hasError}</p>}
-      {Array.from({ length: maxStars }, (_, index) => (
-        <span
-          key={index}
-          className={`star text-3xl cursor-pointer ${
-            index < (rating || 0) ? "text-yellow-400" : "text-gray-400"
-          }`}
-          onClick={() => handleStarClick(index)}
-        >
-          &#9733;
-        </span>
-      ))}
-      <p className="rating-text ml-2">
-        {rating !== null ? `${rating}/${maxStars} stars` : "Please rate"}
-      </p>
+      <div className="rating flex-col items-center justify-center w-fit mx-auto text-center">
+        {hasError && (
+          <p className="pb-2 text-red-500 p-2 text-center text-sm">
+            {hasError}
+          </p>
+        )}
+        {Array.from({ length: maxStars }, (_, index) => (
+          <span
+            key={index}
+            className={`star text-3xl cursor-pointer ${
+              index < (rating || 0) ? "text-yellow-400" : "text-gray-400"
+            }`}
+            onClick={() => handleStarClick(index)}
+          >
+            &#9733;
+          </span>
+        ))}
+        <p className="rating-text ml-2">
+          {rating !== null ? `${rating}/${maxStars} stars` : "Please rate"}
+        </p>
       </div>
       <div className="w-full flex items-center justify-between mt-4">
         <button
@@ -95,11 +102,10 @@ const AddRating: React.FC<RatingProps> = ({
           onClick={handleAddRating}
           disabled={rating === null || isLoading}
         >
-          {isLoading? 'Loading...': 'Add Rating'}
+          {isLoading ? "Loading..." : "Add Rating"}
         </button>
       </div>
     </>
-    
   );
 };
 
