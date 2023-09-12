921de6e75eea5f6299a72f9583668927f9e790ed
raihan2bd <raihan2bd.official@gmail.com>
2023-09-12 16:17:06 +0600

    Add token on every Request of feature movies

--- src/redux/services/featureMoviesApi.ts
+++ src/redux/services/featureMoviesApi.ts
@@ -1,10 +1,23 @@
 import { createApi, fetchBaseQuery, BaseQueryFn, FetchArgs} from '@reduxjs/toolkit/query/react'
+import { getTokenFromLocalStorage } from '../../utils/getToken';
 
 const baseApiUrl = import.meta.env.VITE_API_BASE_URL;
 
+const customBaseQuery = fetchBaseQuery({
+  baseUrl: baseApiUrl,
+  prepareHeaders(headers) {
+    // Get the token and add it to the "Authorization" header.
+    const token = getTokenFromLocalStorage();
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+}) as BaseQueryFn<string | FetchArgs, unknown, CustomErrorType, {}>;
+
 export const featureMoviesApi = createApi({
   reducerPath: 'feature-movies',
-  baseQuery: fetchBaseQuery({baseUrl: baseApiUrl}) as BaseQueryFn<string | FetchArgs, unknown, CustomErrorType, {}>,
+  baseQuery: customBaseQuery,
   tagTypes: ['Feature-Movies'],
   endpoints: (builder) => ({
     getFeatureMovies: builder.query<MoviesResponse, void>({
