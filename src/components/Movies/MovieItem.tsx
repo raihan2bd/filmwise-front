f5430bb07a17607f36a04ebe44225c2c417578e2
raihan2bd <raihan2bd.official@gmail.com>
2023-09-13 21:42:18 +0600

    Fix manage favorite from movieItems

--- src/components/Movies/MovieItem.tsx
+++ src/components/Movies/MovieItem.tsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { logoutAction } from "../../redux/features/authSlice";
+import { useAppSelector, useAppDispatch } from "../../hooks/typeHooks";
+import { useManageFavoriteMutation } from "../../redux/services/movieApi";
 import FavoriteSVG from "../UI/FavoriteSVG";
 
 interface PropsType {
@@ -7,6 +11,38 @@ interface PropsType {
 
 const MovieItem = ({ item }: PropsType) => {
   const baseApiUrl = import.meta.env.VITE_API_BASE_URL;
+  const [manageFavorite, {isLoading: isFavLoading}] = useManageFavoriteMutation()
+
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.auth.user);
+  const { pathname, search } = useLocation();
+  const navigate = useNavigate();
+
+  const handleAddFavorite = async (id: number) => {
+    if (!user) {
+      navigate(`/auth?callback=${pathname}${search}`);
+      return;
+    }
+
+    try {
+      await manageFavorite(id).unwrap()
+    } catch (error) {
+      const err = error as CustomErrorType
+      let errMsg = 'something went wrong. please try again!'
+      if(err.status && err.status === 401) {
+        dispatch(logoutAction())
+      }
+
+      if(err.data.error.message) {
+        errMsg = err.data.error.message
+      }
+
+      // call the ui action later
+      console.log(errMsg)
+    }
+    
+  }
+
   return (
     <li
       className="item bg-white/10 m-2 flex flex-col gap-3 flex-grow max-w-[400px]"
@@ -32,7 +68,7 @@ const MovieItem = ({ item }: PropsType) => {
       </p>
       <div className="flex flex-row justify-between items-center text-sm bg-black/30 px-4 py-2">
         <p className="flex justify-center items-center">
-          <button className="text-2xl text-red-500 hover:bg-red-500 active:bg-red-500 p-2 hover:text-white active:text-white">
+          <button onClick={() => handleAddFavorite(item.id)} disabled={isFavLoading} className="text-2xl text-red-500 hover:bg-red-500 active:bg-red-500 p-2 hover:text-white active:text-white">
             {item.is_favorite ? (
               <FavoriteSVG isFavorite={true} />
             ) : (
