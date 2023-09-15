47de244dede95a1ad02dbfeebb4605763334ceb8
raihan2bd <raihan2bd.official@gmail.com>
2023-09-15 18:12:15 +0600

    Add new movie functionality

--- src/components/Movies/AddOrUpdateMovie.tsx
+++ src/components/Movies/AddOrUpdateMovie.tsx
@@ -3,6 +3,7 @@ import Input from '../UI/Input';
 import Button from '../UI/Button';
 import useInput from '../../hooks/useInput';
 import { validateTitle } from '../../utils/validator';
+import { useAddNewMovieMutation } from '../../redux/services/movieApi';
 
 interface PropsType {
   movieId: number | null;
@@ -11,6 +12,8 @@ interface PropsType {
 const AddOrUpdateMovie = ({movieId}: PropsType) => {
   const [movieForm, setMovieForm] = useState({})
   // const [title, setTitle] = useState('')
+
+  const [addNewMovie, {isLoading: loadingNewMovie}] = useAddNewMovieMutation()
 
 
   const validateTitleInput = (value: string) => {
@@ -43,12 +46,32 @@ const AddOrUpdateMovie = ({movieId}: PropsType) => {
   //   setTitle(e.target.value)
   // }
 
-  const handleOnSubmit = (e: React.FormEvent) => {
+  const handleOnSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if(!formIsValid) {
       return
     }
-    console.log(title)
+
+    const newMovie: MovieInputType = {
+      title: title,
+      description: "lorem ipsumfkdfdkkdfd",
+      runtime: "152",
+      release_date: "2023-09-07",
+      genres: {
+        "3": "Action",
+        "5": "Sci-Fi"
+      },
+      image_id: "1",
+      year: "2023"
+    }
+
+    try {
+      const response = await addNewMovie(newMovie).unwrap()
+      console.log(response)
+    } catch (err) {
+      const error = err as CustomErrorType
+      console.log(error)
+    }
   }
 
 
@@ -67,7 +90,7 @@ const AddOrUpdateMovie = ({movieId}: PropsType) => {
     <form className="w-[800px] max-w-[100%] p-4 bg-white/10" onSubmit={handleOnSubmit}>
       <h3 className="text-2xl text-center my-2 font-bold">{movieId? "Update Movie" : "New Movie"}</h3>
       <Input name="title" label='Title' placeholder="Enter the movie title here" type="text" onChange={titleChangeHandler} inputError={istitleTouched? titleError : null} value={title} onBlur={titleBlurHandler} />
-      <Button disabled={!formIsValid} btnClass="my-3 block ms-auto" type='submit'>Submit</Button>
+      <Button disabled={!formIsValid || loadingNewMovie} btnClass="my-3 block ms-auto" type='submit'>Submit</Button>
     </form>
   )
 }
